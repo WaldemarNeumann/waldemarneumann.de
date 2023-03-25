4f900d8fa81c7b52714d0c4f366b0ab8fe7f16ac
WaldemarNeumann <113894308+WaldemarNeumann@users.noreply.github.com>
2023-03-25 18:21:00 +0100

    Create slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'waldemarneumann';
+export class AppComponent implements OnInit {
+  images = ['maschinenbau.jpg', 'rechner.jpg', 'software.jpg'];
+  currentimage = 0;
+  showImage = false;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentimage++;
+      this.currentimage = this.currentimage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+
+  }
 }
